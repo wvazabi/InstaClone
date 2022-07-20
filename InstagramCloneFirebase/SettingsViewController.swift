3abccf141d89849fcf99a6fd52ccea0e2f44d774
Enes Kaya <eng.eneskaya@gmail.com>
2022-07-20 23:16:56 +0300

    Settings View Controller

---
+++ InstagramCloneFirebase/SettingsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SettingsViewController.swift
+//  InstagramCloneFirebase
+//
+//  Created by Enes Kaya on 20.07.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func logoutClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toViewControllar", sender: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
