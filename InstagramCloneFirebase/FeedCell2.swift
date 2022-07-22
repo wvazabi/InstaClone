ec56f89b7373d1cad69f1ae43a962bb744767de9
Enes Kaya <eng.eneskaya@gmail.com>
2022-07-22 17:17:40 +0300

    Like button heart

--- InstagramCloneFirebase/FeedCell2.swift
+++ InstagramCloneFirebase/FeedCell2.swift
@@ -14,16 +14,24 @@ class FeedCell2: UITableViewCell {
 
     @IBOutlet weak var userEmailLabel: UILabel!
     
+    @IBOutlet weak var buttonLike: UIButton!
     @IBOutlet weak var documentIDLabel: UILabel!
     
     @IBOutlet weak var userImageView: UIImageView!
     
     @IBOutlet weak var userComment: UILabel!
     
+    var userLikedClickedArray = [String]()
+    
+   
+    var currentUser = Auth.auth().currentUser!.email!
+    
+    
     @IBOutlet weak var likeCountLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        buttonLike.setImage(UIImage(systemName: "suit.heart"), for: .normal)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -35,13 +43,32 @@ class FeedCell2: UITableViewCell {
     
     @IBAction func likeClickedButton(_ sender: Any) {
         
+        let dummy = "User: \(currentUser)"
         let firestore = Firestore.firestore()
         
-        if let likeCount  = Int(likeCountLabel.text!){
-            
-            let likeStore = ["likes" : likeCount + 1] as [String : Any]
         
-            firestore.collection("Posts").document(documentIDLabel.text!).setData(likeStore, merge: true)
+        if (dummy != userEmailLabel.text){
+            
+            if  buttonLike.tag == 0{
+                buttonLike.setImage(UIImage(systemName: "suit.heart.fill"), for: .normal)
+                
+                if let likeCount  = Int(likeCountLabel.text!){
+                    
+                    let likeStore = ["likes" : likeCount + 1] as [String : Any]
+                    
+                    buttonLike.tag = 1
+                
+                    firestore.collection("Posts").document(documentIDLabel.text!).setData(likeStore, merge: true)
+                
+                    }
+                
+                
+           
+                
+                
+            }
+        
+       
         
         
         }
