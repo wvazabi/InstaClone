78ac1c081cde6eebcb1158d64442f6e802b5be93
Enes Kaya <eng.eneskaya@gmail.com>
2022-07-22 11:37:17 +0300

    Fetch Data

--- InstagramCloneFirebase/FeedViewController.swift
+++ InstagramCloneFirebase/FeedViewController.swift
@@ -6,31 +6,25 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
+
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
  
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell2
-        cell.userComment.text = "comment"
-        cell.userEmailLabel.text = "user@email.com"
-        cell.userImageView.image = UIImage(named: "select")
-        cell.likeCountLabel.text = "0"
-            
-        
-        return cell
-    }
+  
+
     
     
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var userEmailArray = [String]()
+    var userCommentArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,8 +32,82 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.delegate = self
         tableView.dataSource = self
 
-        
+        getDataFromfirestore()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    func alertPopUp(titleInput:String, messageInput:String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userEmailArray.count
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell2
+        cell.userComment.text = userCommentArray[indexPath.row]
+        cell.userEmailLabel.text = userEmailArray[indexPath.row]
+        cell.userImageView.image = UIImage(named: "select")
+        cell.likeCountLabel.text = String(likeArray[indexPath.row])
+            
+        
+        return cell
+    }
+    
+    
+    func getDataFromfirestore(){
+        
+        let firestoreDataBase = Firestore.firestore()
+        
+       // let settings = firestoreDataBase.settings
+        
+        firestoreDataBase.collection("Posts").addSnapshotListener { snapshot, error in
+            if error != nil {
+                self.alertPopUp(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+            }else{
+                
+                if snapshot != nil && snapshot?.isEmpty != true {
+                    
+                    for document in snapshot!.documents {
+                         
+                        if let postedBy = document.get("postComment") as? String {
+                            
+                            self.userEmailArray.append(postedBy)
+                            
+                            if let comment = document.get("comment") as? String{
+                                self.userCommentArray.append(comment)
+                                
+                                if let likes = document.get("likes") as? Int{
+                                    self.likeArray.append(likes)
+                                    
+                                    if let imageUrl = document.get("imageUrl") as? String{
+                                        self.userImageArray.append(imageUrl)
+                                    }
+                                    
+                                }
+                            }
+                            
+                        }
+                        
+                        self.tableView.reloadData()
+                    }
+                }
+                
+                
+            }
+        }
+        
     }
     
 
